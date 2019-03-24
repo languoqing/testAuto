e3251ccf2d10127a61f68d8d0c3acea5ee85ce66
languoqing <jinglan1014@163.com>
2019-03-24 10:05:31 +0800

    增加余额测试用例

--- src/test/java/TestGameYaoYiYao.java
+++ src/test/java/TestGameYaoYiYao.java
@@ -20,15 +20,15 @@ public class TestGameYaoYiYao {
     static void beforAll(){
         mainPage = MainPage.startApp();
         searchGamePage = mainPage.goSearchGamePage();
+        gameYaoYiYao = (GameYaoYiYao)searchGamePage.goGamePage("超级摇一摇");//Object 对象转换
+        if (Fuctions.isElementExist(CommnPage.goGamePage)){
+            CommnPage.outTranPage();//点击过渡页
+        }
     }
 
     @Test
     @DisplayName("点击余额")
      void testAShowAccountBlance(){
-        gameYaoYiYao = (GameYaoYiYao)searchGamePage.goGamePage("超级摇一摇");//Object 对象转换
-        if (Fuctions.isElementExist(CommnPage.goGamePage)){
-            CommnPage.outTranPage();//点击过渡页
-        }
         Assertions.assertTrue(gameYaoYiYao.clikAccountBlance());
     }
 
@@ -36,6 +36,20 @@ public class TestGameYaoYiYao {
     @DisplayName("关闭余额显示")
     void testCloseAccountBlance(){
         Assertions.assertFalse(gameYaoYiYao.clickCloseAccountBlance());
+    }
+
+    @Test
+    @DisplayName("显示欢乐豆值")
+    void testTCoinDetail(){
+        String detail = gameYaoYiYao.getTcoinDetail();
+        Assertions.assertTrue(Integer.parseInt(detail) >= 0);
+    }
+
+    @Test
+    @DisplayName("显示余额值")
+    void testAccountBlanceDetail(){
+        String accountDetail = gameYaoYiYao.getAccountBlanceDetail();
+        Assertions.assertTrue(Integer.parseInt(accountDetail) >= 0);
     }
 
     @AfterAll
